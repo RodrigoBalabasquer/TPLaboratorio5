0497c83ef29b3ba47648bd7df011bf3d202435eb
RodrigoBalabasquer <r.balabasquer@gmail.com>
2019-06-01 18:41:47 -0300

    Incorporo webview y menu (buscador y retroceso)

--- app/src/main/java/com/example/alumno/tplaboratoriov/AnotherActivity.java
+++ app/src/main/java/com/example/alumno/tplaboratoriov/AnotherActivity.java
@@ -1,10 +1,15 @@
 package com.example.alumno.tplaboratoriov;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class AnotherActivity extends AppCompatActivity implements  View.OnClickListener {
@@ -13,16 +18,40 @@ public class AnotherActivity extends AppCompatActivity implements  View.OnClickL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("asdda","Se hizo click en el menu ");
         setContentView(R.layout.activity_another);
-
-        b = (Button) this.findViewById(R.id.btn);
-        b.setOnClickListener(this);
-
         Intent i = getIntent();
-        b.setText(i.getStringExtra("link"));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(i.getStringExtra("title"));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //b = (Button) this.findViewById(R.id.btn);
+        //b.setOnClickListener(this);
+        //b.setText(i.getStringExtra("link"));
+        WebView wv = (WebView)super.findViewById(R.id.web);
+        WebSettings ws = wv.getSettings();
+        ws.setJavaScriptEnabled(true);
+
+        wv.loadUrl(i.getStringExtra("link"));
     }
     @Override
     public void onClick(View v) {
         this.finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu2,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        if(menuItem.getItemId() == R.id.opcion1){
+            Log.d("Click Menu","Se hizo click en el menu "+menuItem.getTitle());
+        }
+        else  if(menuItem.getItemId() == android.R.id.home){
+            this.finish();
+        }
+        return  super.onOptionsItemSelected(menuItem);
     }
 }
--- app/src/main/java/com/example/alumno/tplaboratoriov/MainActivity.java
+++ app/src/main/java/com/example/alumno/tplaboratoriov/MainActivity.java
@@ -7,16 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements MyOnItemClick, Handler.Callback {
+public class MainActivity extends AppCompatActivity implements MyOnItemClick, Handler.Callback,SearchView.OnQueryTextListener {
     MyAdapter adapter;
     List<Item> items;
+    List<Item> itemsBackUp;
     public static final int TEXTO = 1;
     public static final int IMAGEN = 2;
     @Override
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements MyOnItemClick, Ha
         setContentView(R.layout.activity_main);
 
         items = new ArrayList<>();
+        itemsBackUp = new ArrayList<>();
 
         RecyclerView rvPersona = (RecyclerView) super.findViewById(R.id.rvPersonas);
 
@@ -47,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements MyOnItemClick, Ha
         Intent i = new Intent(this,AnotherActivity.class);
 
         i.putExtra("link",item.getLink());
+        i.putExtra("title",item.getTitle());
         this.startActivity(i);
 
     }
@@ -56,11 +62,63 @@ public class MainActivity extends AppCompatActivity implements MyOnItemClick, Ha
         if(msg.arg1 == TEXTO){
             this.items = this.adapter.SetPersonas((List<Item>) msg.obj);
             adapter.notifyDataSetChanged();
+            this.itemsBackUp = (List<Item>) msg.obj;
         }
         else{
             this.adapter.SetImagenPer((byte[])msg.obj,msg.arg2);
             adapter.notifyItemChanged(msg.arg2);
         }
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        MenuItem mi = menu.findItem(R.id.opcion1);
+        SearchView sv = (SearchView)mi.getActionView();
+        sv.setOnQueryTextListener(this);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        if(menuItem.getItemId() == R.id.opcion1){
+            Log.d("Click Menu","Se hizo click en el menu "+menuItem.getTitle());
+        }
+        return  super.onOptionsItemSelected(menuItem);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String newText) {
+        Log.d("Enter",newText);
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Log.d("Escribo",newText);
+        if(newText.length() > 3) {
+            this.filtrar(newText);
+            this.items = adapter.SetPersonas(this.items);
+            adapter.notifyDataSetChanged();
+        }
+        else{
+            this.items = this.itemsBackUp;
+            adapter.SetPersonas(this.itemsBackUp);
+            adapter.notifyDataSetChanged();
+        }
+        return false;
+    }
+
+    public void filtrar(String filtro){
+        this.items = new ArrayList<Item>();
+
+        for (Item item: this.itemsBackUp) {
+            Log.d("Titulo",item.getTitle());
+            if(item.getTitle().toLowerCase().contains(filtro.toLowerCase())){
+                Log.d("Filro","oka");
+                this.items.add(item);
+            }
+        }
     }
 }
